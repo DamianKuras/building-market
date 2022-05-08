da9ff67ff5498efba5f02b4d785d561e231f21d4
DamianKuras <damian_kuras@outlook.com>
2022-05-08 21:35:56 +0200

    removing font awesome and replacing it with svg

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -12,7 +12,6 @@ use app\base\Application;
     <meta name="author" content="Damian Kuraś">
     <link rel="stylesheet" href="/css/style.css">
     <title><?php echo $this->title ?></title>
-    <script src="https://kit.fontawesome.com/e4e6c7b251.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="icon" href="/assets/favicon.ico">
